e5bdc3f859242a5bf8d94a875d1156bf047017a0
Sagar Replit <sagargoel2907@gmail.com>
2023-06-12 13:21:26 +0000

    initial-setup done

---
+++ src/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('running');
+  if (localStorage.getItem('accessKey')) {
+    window.location.href = 'dashboard/dashboard.html';
+  }
+  else {
+    window.location.href = 'login/login.html';
+  }
+});
