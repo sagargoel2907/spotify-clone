b707bbbbc5e6dab5f210ced262563d1b33e6beb9
sagar replit <sagargoel@replit.com>
2023-06-14 14:40:15 +0000

    updated code

--- src/login/login.js
+++ src/login/login.js
@@ -1,13 +1,13 @@
 const CLIENT_ID = "ea1dcd85f8094f67b84892e19dbbe6f5";
 const scopes = "user-top-read user-follow-read playlist-read-private user-library-read";
-const REDIRECT_URI = "http://localhost:3000/login/login.html";
+const REDIRECT_URI = "spotify-clone.sagargoel.repl.co";
 
 const authorizeUser = () => {
-    const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scopes=${scopes}&show_dialog=true`;
-    window.open(url, "login", "width=800,height=600");
+  const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scopes=${scopes}&show_dialog=true`;
+  window.open(url, "login", "width=800,height=600");
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementById("login-to-spotify");
-    loginButton.addEventListener("click", authorizeUser);
+  const loginButton = document.getElementById("login-to-spotify");
+  loginButton.addEventListener("click", authorizeUser);
 });
