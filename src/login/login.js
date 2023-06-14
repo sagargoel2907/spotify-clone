78c3cf3be969744960a6550491a38de2962b8aa7
sagar_goel <sagar_goel@hcl.com>
2023-06-14 20:12:38 +0530

    added code

--- src/login/login.js
+++ src/login/login.js
@@ -1,6 +1,8 @@
 const CLIENT_ID = "ea1dcd85f8094f67b84892e19dbbe6f5";
 const scopes = "user-top-read user-follow-read playlist-read-private user-library-read";
 const REDIRECT_URI = "http://localhost:3000/login/login.html";
+const ACCESS_TOKEN_KEY = "accessToken";
+const APP_URL="spotify-clone.sagargoel.repl.com";
 
 const authorizeUser = () => {
     const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scopes=${scopes}&show_dialog=true`;
@@ -10,4 +12,11 @@ const authorizeUser = () => {
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById("login-to-spotify");
     loginButton.addEventListener("click", authorizeUser);
+});
+
+window.addEventListener("onload", () => {
+    const accessToken = localStorage.getItem("accessToken");
+    if(accessToken){
+
+    }
 });
