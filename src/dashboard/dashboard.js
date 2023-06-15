fdb07f7dcfc29b6de0197a796e9fbe56a9ab22f7
sagar replit <sagargoel@replit.com>
2023-06-15 20:33:31 +0000

    added some code

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -2,13 +2,19 @@ import { fetchData } from "../api";
 import { ENDPOINTS } from "../common";
 
 const loadUserProfile = async () => {
-    const defaultImage = document.querySelector("default-image");
-    const userProfileBtn = document.querySelector("user-profile-btn");
-    const displayName = document.querySelector("display-name");
+    const defaultImage = document.querySelector("#default-image");
+    const userProfileBtn = document.querySelector("#user-profile-btn");
+    const displayNameElement = document.querySelector("#display-name");
 
     const userProfile = await fetchData(ENDPOINTS.userProfile);
-    console.log(userProfile);
-    alert(userProfile);
+    const { display_name: displayName, images } = userProfile;
+    displayNameElement.textContent = displayName;
+
+    if(images.length){
+        defaultImage.classList.add("hidden");
+    }else{
+        defaultImage.classList.remove("hidden");
+    }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
