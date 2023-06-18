7658148a6615adcdf776f8904c7f4b8afd580299
sagar replit <sagargoel@replit.com>
2023-06-18 20:25:50 +0000

    added line clamp dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        "green": "#1db954",
-        "black-base": "#121212",
-        "black-primary": "#191414",
-        "black-secondary": "#171818",
-        "primary": "#ffffff",
-        "secondary": "#b3b3b3",
-        "gray": "#535353"
-      },
-      gridTemplateColumns: {
-        "auto-fill-card": "repeat(auto-fill,minmax(200px,1fr))"
-      }
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "green": "#1db954",
+                "black-base": "#121212",
+                "black-primary": "#191414",
+                "black-secondary": "#171818",
+                "primary": "#ffffff",
+                "secondary": "#b3b3b3",
+                "gray": "#535353"
+            },
+            gridTemplateColumns: {
+                "auto-fill-card": "repeat(auto-fill,minmax(200px,1fr))"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [require('@tailwindcss/line-clamp')],
 }
 
